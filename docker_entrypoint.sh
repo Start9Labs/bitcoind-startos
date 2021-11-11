b14454202a94e4931720493aefd3e389ce0ba444
Chris Guida <chrisguida@gmail.com>
2021-11-11 11:21:23 -0600

    allow pruning to be disabled; fix backups and health checks

--- docker_entrypoint.sh
+++ docker_entrypoint.sh
@@ -2,7 +2,7 @@
 
 set -euo pipefail
 
-export HOST_IP=$(ip -4 route list match 0/0 | awk '{print $3}')
+export EMBASSY_IP=$(ip -4 route list match 0/0 | awk '{print $3}')
 export PEER_TOR_ADDRESS=$(yq e '.peer-tor-address' /root/.bitcoin/start9/config.yaml)
 export RPC_TOR_ADDRESS=$(yq e '.rpc-tor-address' /root/.bitcoin/start9/config.yaml)
 
