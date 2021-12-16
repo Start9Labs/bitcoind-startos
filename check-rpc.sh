0d3fcd6a8674fe8de584c40c994e27ebb1ded7a2
Chris Guida <chrisguida@users.noreply.github.com>
2021-12-16 14:15:36 -0600

    attempt to fix blank error messages (#29)
    * attempt to fix blank error messages
    
    * address pr comments, remove exit 0 since that happens anyway

--- check-rpc.sh
+++ check-rpc.sh
@@ -5,9 +5,10 @@
 gi_result=$(bitcoin-cli -getinfo 2>&1)
 error_code=$?
 
-if [[ "$error_code" == "28" ]]; then
+if [ "$error_code" -eq 28 ]; then
     # Starting
     exit 60
+else
+    echo $gi_result >&2
+    exit $error_code
 fi
-
-exit $error_code
