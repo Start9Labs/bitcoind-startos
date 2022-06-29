a7f886b7e30c861b899377dad5547d07aaddfa98
kn0wmad <kn0wmad@protonmail.com>
2022-06-29 14:16:46 -0600

    Fixed v21 migration

---
+++ migrations/lt_22_0_0.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -ea
+
+if [ $1 = "from" ]; then 
+    yq -i '.advanced.peers.addnode |= map(select(.hostname != ~ or . == "*"))' /root/.bitcoin/start9/config.yaml
+    yq -i '.advanced.peers.addnode.[] |= {"hostname":., "port":~}' /root/.bitcoin/start9/config.yaml
+    yq -i '(.advanced.blockfilters.blockfilterindex, .advanced.blockfilters.peerblockfilters, .advanced.bloomfilters.peerbloomfilters) = false' /root/.bitcoin/start9/config.yaml
+    echo '{"configured": true }'
+    exit 0
+elif [ $1 = "to" ]; then
+    yq -i '.advanced.peers.addnode |= map_values(.hostname)' /root/.bitcoin/start9/config.yaml
+    yq -i 'del(.advanced.blockfilters)' /root/.bitcoin/start9/config.yaml
+    yq -i 'del(.advanced.bloomfilters)' /root/.bitcoin/start9/config.yaml
+    echo '{"configured": false }'
+    exit 0
+else
+    echo "FATAL: Invalid argument: {from, to}. Migration failed." >&2
+    exit 1
+fi
+``
