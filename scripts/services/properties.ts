488af7366bf2035b20c8bd1e008d341f5eb86b69
J M <mogulslayer@gmail.com>
2022-06-29 09:47:21 -0600

    fix: missing properties

--- scripts/services/properties.ts
+++ scripts/services/properties.ts
@@ -1,14 +1,29 @@
 import { Effects, ExpectedExports, YAML } from "../deps.ts";
 
-export const properties: ExpectedExports.properties = async (
+type UnPromise<A> = A extends Promise<infer B> ? B : never
+// deno-lint-ignore no-explicit-any
+const asResult = (result: any) => ({ result })
+const noPropertiesFound: UnPromise<ReturnType<ExpectedExports.properties>> = {
+  result: {
+    version: 2,
+    data: {
+      "Not Ready": {
+        type: "string",
+        value: "Could not find properties. The service might still be starting",
+        qr: false,
+        copyable: false,
+        masked: false,
+        description: "Fallback Message When Properties could not be found"
+      }
+    }
+  }
+} as const
+export const properties: ExpectedExports.properties = (
   effects: Effects,
 ) => {
-  return {
-    result: YAML.parse(
-      await effects.readFile({
-        path: "start9/stats.yaml",
-        volumeId: "main",
-      }),
-    ) as any,
-  };
+
+  return effects.readFile({
+    path: "start9/stats.yaml",
+    volumeId: "main",
+  }).then(YAML.parse).then(asResult, () => noPropertiesFound)
 };
