16d4ac13aa86d8490dc05507205b490937b41a44
J M <2364004+Blu-J@users.noreply.github.com>
2022-06-29 12:54:58 -0600

    fix: missing properties (#75)
    * fix: missing properties
    
    * chore: Convert to compat
    
    * chore: Update the sdk
    
    * Update dependencies.ts

--- scripts/services/properties.ts
+++ scripts/services/properties.ts
@@ -1,14 +1,2 @@
-import { Effects, ExpectedExports, YAML } from "../deps.ts";
-
-export const properties: ExpectedExports.properties = async (
-  effects: Effects,
-) => {
-  return {
-    result: YAML.parse(
-      await effects.readFile({
-        path: "start9/stats.yaml",
-        volumeId: "main",
-      }),
-    ) as any,
-  };
-};
+import { compat } from "../dependencies.ts";
+export const properties = compat.properties
