a9238f2c2afb2e7d83a2ded992758a069c96c9a7
J M <mogulslayer@gmail.com>
2022-07-01 13:01:15 -0600

    chore: add in results for the no txindex

--- scripts/services/action.ts
+++ scripts/services/action.ts
@@ -1,11 +1,22 @@
-import { types as T } from '../dependencies.ts'
+import { exists, types as T } from '../dependencies.ts'
 
 export const action = {
     async "delete-txindex"(effect: T.Effects, _input?: T.Config): Promise<T.ResultType<T.ActionResult>> {
-        await effect.removeDir({
+        const txinfoLocation = {
             path: "indexes/txindex",
             volumeId: "main",
-        });
+        }
+        if (await exists(effect, txinfoLocation) === false) {
+            return {
+                result: {
+                    copyable: false,
+                    message: "Txindex doesn't exist",
+                    version: "0",
+                    qr: false,
+                }
+            }
+        }
+        await effect.removeDir(txinfoLocation);
         return {
             result: {
                 copyable: false,
