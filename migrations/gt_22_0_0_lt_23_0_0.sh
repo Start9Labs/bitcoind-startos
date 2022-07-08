5c1625d312a2ec58cfa6fe5ff1d597fcee47f1d8
kn0wmad <kn0wmad@protonmail.com>
2022-07-08 09:40:44 -0600

    Fixed migrations

--- migrations/gt_22_0_0_lt_23_0_0.sh
+++ migrations/gt_22_0_0_lt_23_0_0.sh
@@ -5,8 +5,8 @@ set -ea
 if [ $1 = "from" ]; then 
     yq -i '.advanced.peers.addnode |= map(select(.hostname != ~ or . == "*"))' /root/.bitcoin/start9/config.yaml
     yq -i '.advanced.peers.addnode.[] |= {"hostname":., "port":~}' /root/.bitcoin/start9/config.yaml
-    yq -i '(.advanced.blockfilters.blockfilterindex, .advanced.blockfilters.peerblockfilters, .advanced.bloomfilters.peerbloomfilters) = false' /root/.bitcoin/start9/config.yaml
-    echo '{"configured": true }'
+    yq -i '(.advanced.bloomfilters.peerbloomfilters) = false' /root/.bitcoin/start9/config.yaml
+    echo '{"configured": false }'
     exit 0
 elif [ $1 = "to" ]; then
     yq -i '.advanced.peers.addnode |= map_values(.hostname)' /root/.bitcoin/start9/config.yaml
