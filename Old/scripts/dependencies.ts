aa837cda9a1edeb18d17d126f5d762c848052b34
Shadowy Super Coder <musashidisciple@proton.me>
2025-02-25 08:59:04 -0700

    remove old dir

--- Old/scripts/dependencies.ts
+++
@@ -1,1 +0,0 @@
-export * from "https://deno.land/x/embassyd_sdk@v0.3.4.3.0-alpha1/mod.ts";
--- Old/scripts/embassy.ts
+++
@@ -1,6 +0,0 @@
-// @ts-check
-export { setConfig } from "./services/setConfig.ts";
-export { properties } from "./services/properties.ts";
-export { getConfig } from "./services/getConfig.ts";
-export { action } from "./services/action.ts";
-export { migration } from "./services/migrations.ts";
