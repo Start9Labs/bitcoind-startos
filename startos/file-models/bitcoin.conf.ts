4e2a77d0a8fdf1b604a299c52daf1dc72fd7aab6
Matt Hill <mattnine@protonmail.com>
2025-05-01 10:20:37 -0600

    beta 12

--- startos/file-models/bitcoin.conf.ts
+++ startos/file-models/bitcoin.conf.ts
@@ -1,8 +1,8 @@
 import { FileHelper, matches } from '@start9labs/start-sdk'
 import { bitcoinConfDefaults } from '../utils'
 
-const { anyOf } = matches
-const object = matches.object
+const { anyOf, arrayOf, object } = matches
+
 const stringArray = matches.array(matches.string)
 const string = stringArray.map(([a]) => a).orParser(matches.string)
 const number = stringArray.map(([a]) => Number(a)).orParser(matches.number)
@@ -17,6 +17,7 @@ const literal = (val: string) => {
     .map(([val]) => matches.literal(val))
     .orParser(matches.literal(val))
 }
+
 const {
   rpcbind,
   rpcallowip,
@@ -79,7 +80,7 @@ export const shape = object({
   // Peers
   listen: boolean.onMismatch(listen),
   bind: string.optional().onMismatch(bind),
-  connect: anyOf(stringArray, numLiteral(0)).onMismatch(connect),
+  connect: arrayOf(string).optional().onMismatch(connect),
   addnode: anyOf(stringArray).optional().onMismatch(addnode),
   onlynet: string.optional().onMismatch(onlynet),
   v2transport: boolean.onMismatch(v2transport),
@@ -120,58 +121,7 @@ export const shape = object({
   peerblockfilters: boolean.onMismatch(peerblockfilters),
 })
 
-export function fromBitcoinConf(text: string): Record<string, string[]> {
-  const lines = text.split('\n')
-  const dictionary = {} as Record<string, string[]>
-
-  for (const line of lines) {
-    const [key, value] = line.split('=', 2)
-    if (key === '') {
-      return dictionary
-    } else if (key.startsWith('#')) {
-      continue
-    }
-    const trimmedKey = key.trim()
-    const trimmedValue = value.trim()
-
-    if (!dictionary[trimmedKey]) {
-      dictionary[trimmedKey] = []
-    }
-
-    dictionary[trimmedKey].push(trimmedValue)
-  }
-
-  return dictionary
-}
-
-function toBitcoinConf(conf: typeof shape._TYPE): string {
-  let bitcoinConfStr = ''
-  const toString = (a: any) => {
-    if (a === true) {
-      return '1'
-    } else if (a === false) {
-      return '0'
-    } else {
-      return a.toString()
-    }
-  }
-
-  Object.entries(conf).forEach(([key, value]) => {
-    if (Array.isArray(value)) {
-      for (const subValue of value) {
-        bitcoinConfStr += `${key}=${toString(subValue)}\n`
-      }
-    } else if (value !== undefined) {
-      bitcoinConfStr += `${key}=${toString(value)}\n`
-    }
-  })
-
-  return bitcoinConfStr
-}
-
-export const bitcoinConfFile = FileHelper.raw(
+export const bitcoinConfFile = FileHelper.ini(
   '/media/startos/volumes/main/bitcoin.conf',
-  (obj: typeof shape._TYPE) => toBitcoinConf(obj),
-  (str) => fromBitcoinConf(str),
-  (obj) => shape.withMismatch((_) => shape.unsafeCast({})).unsafeCast(obj),
+  shape,
 )
