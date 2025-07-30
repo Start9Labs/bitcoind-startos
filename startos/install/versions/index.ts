1352c4f01c320ffa077be4d8f446cddbdd6f7141
Shadowy Super Coder <musashidisciple@proton.me>
2025-07-30 14:53:10 -0600

    alpha.8

--- startos/install/versions/index.ts
+++ startos/install/versions/index.ts
@@ -1,4 +1,4 @@
-export { v28_1_0_3_alpha7 as coreCurrent } from './v28_1_0_3-alpha.7'
+export { v28_1_0_3_alpha8 as coreCurrent } from './v28_1_0_3-alpha.8'
 import { v28_1_0_3 } from './v28_1_0_3'
 
 export const other = [v28_1_0_3]
--- startos/install/versions/v28_1_0_3-alpha.8.ts
+++ startos/install/versions/v28_1_0_3-alpha.8.ts
@@ -1,7 +1,7 @@
 import { VersionInfo, IMPOSSIBLE } from '@start9labs/start-sdk'
 
-export const v28_1_0_3_alpha7 = VersionInfo.of({
-  version: '28.1:3-alpha.7',
+export const v28_1_0_3_alpha8 = VersionInfo.of({
+  version: '28.1:3-alpha.8',
   releaseNotes: 'Revamped for StartOS 0.4.0',
   migrations: {
     up: async ({ effects }) => {},
