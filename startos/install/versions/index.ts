f7360b0b24ac0a7dce9dd6d273c0819901a71f43
Shadowy Super Coder <musashidisciple@proton.me>
2025-10-03 19:11:40 -0600

    ensure correct defaults on alpha installs

--- startos/install/versions/index.ts
+++ startos/install/versions/index.ts
@@ -1,3 +1,4 @@
-export { v29_1_0_2 as coreCurrent} from './v29_1_0_2-beta.0'
+export { v29_1_0_2_beta1 as coreCurrent} from './v29_1_0_2-beta.1'
+import { v29_1_0_2 } from './v29_1_0_2-beta.0'
 
-export const other = []
+export const other = [v29_1_0_2]
