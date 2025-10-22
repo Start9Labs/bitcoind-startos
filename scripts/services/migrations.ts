de6bc425a78bc8af485d71d1bde158571401f18b
Shadowy Super Coder <musashidisciple@proton.me>
2025-10-22 11:51:54 -0600

    v30 migrations

--- scripts/services/migrations.ts
+++ scripts/services/migrations.ts
@@ -429,6 +429,61 @@ export const migration: T.ExpectedExports.migration =
           type: "down",
         }),
       },
+      "30.0.0": {
+        up: compat.migrations.updateConfig(
+          (config) => {
+            if (
+              matches
+                .shape({
+                  advanced: matches.shape({
+                    mempool: matches.shape({
+                      datacarriersize: matches.any,
+                    }),
+                  }),
+                })
+                .test(config)
+            ) {
+              if (
+                config.advanced.mempool.datacarriersize === 83 ||
+                config.advanced.mempool.datacarriersize === 42
+              ) {
+                config.advanced.mempool.datacarriersize = 100_000;
+              }
+            }
+            return config;
+          },
+          true,
+          {
+            version: "30.0.0",
+            type: "up",
+          }
+        ),
+        down: compat.migrations.updateConfig(
+          (config: any) => {
+            if (
+              matches
+                .shape({
+                  advanced: matches.shape({
+                    mempool: matches.shape({
+                      datacarriersize: matches.any,
+                    }),
+                  }),
+                })
+                .test(config)
+            ) {
+              if (config.advanced.mempool.datacarriersize === 100_000) {
+                config.advanced.mempool.datacarriersize = 83;
+              }
+            }
+            return config;
+          },
+          true,
+          {
+            version: "30.0.0",
+            type: "down",
+          }
+        ),
+      },
     },
-    "29.2.0"
+    "30.0.0"
   );
